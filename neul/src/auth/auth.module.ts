705c11de75020028597e090a2977fca9032d9a88
Yzoraa <yezing01@naver.com>
2025-05-01 14:56:29 +0900

    Feat: 로컬로그인

--- neul/src/auth/auth.module.ts
+++ neul/src/auth/auth.module.ts
@@ -5,16 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'entities/users';
 import { Patients } from 'entities/patients';
 import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './strategy/local';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     JwtModule.register({ // 토큰 발급 기능 등록
       secret: process.env.JWT_SECRET,
       signOptions: {expiresIn: '1d'},
     }),
     TypeOrmModule.forFeature([Users, Patients])
   ],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
   exports: [JwtModule]
 })
