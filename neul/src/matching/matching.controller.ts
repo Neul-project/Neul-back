c34fd081dbe3ae3913942f70fb78aa4204e6f86e
Yzoraa <yezing01@naver.com>
2025-05-13 16:32:34 +0900

    Feat: 담당유저 전달

--- neul/src/matching/matching.controller.ts
+++ neul/src/matching/matching.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { MatchingService } from './matching.service';
 import { ApiBody, ApiResponse } from '@nestjs/swagger';
 import { UserPatientDto } from './dto/res/user-patient.dto';
 import { DeleteStatusDto } from 'src/status/dto/delete-status.dto';
 import { MatchUserDto } from './dto/match-user.dto';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
 
 @Controller('matching')
 export class MatchingController {
@@ -14,6 +15,15 @@ export class MatchingController {
     @ApiResponse({type: UserPatientDto})
     async userAll(){
         return this.matchingService.userAll();   
+    }
+
+    // 담당 유저 전달
+    @Get('/matchuser')
+    @UseGuards(JwtAuthGuard)
+    @ApiResponse({type: UserPatientDto})
+    async userSelected(@Req() req){
+        const userId = req.user.id;
+        return this.matchingService.userSelect(userId);
     }
 
     // 선택한 유저 탈퇴
--- neul/src/matching/matching.service.ts
+++ neul/src/matching/matching.service.ts
@@ -40,6 +40,31 @@ export class MatchingService {
         }));
     }
 
+    // 담당 유저 전달
+    async userSelect(userId: number){
+        const patients = await this.patientRepository.find({
+            where: {admin: {id: userId}},
+            relations: ['user', 'admin']
+        });
+
+        return patients.map(x => ({
+            user_id: x.user?.id || null,
+            user_name: x.user?.name || null,
+            user_email: x.user?.email || null,
+            user_phone: x.user?.phone || null,
+            user_create: x.user?.created_at || null,
+
+            admin_id: x.admin?.id || null,
+            admin_name: x.admin?.name || null,
+
+            patient_id: x.id,
+            patient_name: x.name || '없음',
+            patient_gender: x.gender || '없음',
+            patient_birth: x.birth || '없음',
+            patient_note: x.note || '없음'
+        }));
+    }
+
     // 선택 유저 삭제
     async userDel(ids: number[]){
         return await this.userRepository.delete(ids);
@@ -48,18 +73,11 @@ export class MatchingService {
     // 피보호자-관리자 매칭 + 채팅방 생성
     async userMatch(adminId: number, userId: number, patientId: number){
         const admin = await this.userRepository.findOne({where:{ id: adminId }});
-        if (!admin){
-            throw new NotFoundException('해당 관리자 계정을 찾을 수 없습니다.');
-        }
-
         const user = await this.userRepository.findOne({where: { id: userId }});
-        if (!user) {
-            throw new NotFoundException('해당 보호자 계정을 찾을 수 없습니다.');
-        }
-
         const patient = await this.patientRepository.findOne({where: { id: patientId }});
-        if (!patient){ 
-            throw new NotFoundException('해당 피보호자를 찾을 수 없습니다.');
+        
+        if (!admin || !user || !patient){
+            throw new NotFoundException('해당 관리자/보호자/피보호자 계정을 찾을 수 없습니다.');
         }
 
         patient.admin = admin;
@@ -86,18 +104,11 @@ export class MatchingService {
     // 피보호자-관리자 매칭 취소 + 채팅방 삭제
     async userNotMatch(adminId: number, userId: number, patientId: number){
         const admin = await this.userRepository.findOne({where:{ id: adminId }});
-        if (!admin){
-            throw new NotFoundException('해당 관리자 계정을 찾을 수 없습니다.');
-        }
-
         const user = await this.userRepository.findOne({where: { id: userId }});
-        if (!user) {
-            throw new NotFoundException('해당 보호자 계정을 찾을 수 없습니다.');
-        }
-
         const patient = await this.patientRepository.findOne({where: { id: patientId }});
-        if (!patient){ 
-            throw new NotFoundException('해당 피보호자를 찾을 수 없습니다.');
+        
+        if (!admin || !user || !patient){
+            throw new NotFoundException('해당 관리자/보호자/피보호자 계정을 찾을 수 없습니다.');
         }
 
         patient.admin = null;
