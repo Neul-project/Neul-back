0fb7c933b6b8461bd458867501d8d9f75fa98c2e
Yzoraa <yezing01@naver.com>
2025-05-30 11:01:16 +0900

    Feat: 시간대 변경

--- neul/entities/chats.ts
+++ neul/entities/chats.ts
@@ -27,7 +27,7 @@ export class Chats {
     @ManyToOne(() => ChatRoom, (room) => room.chats, {onDelete: 'CASCADE'})
     room: ChatRoom;
   
-    @CreateDateColumn({ type: 'timestamp' })
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     created_at: Date;
 
     @Column({ comment: '사용자 삭제', default: false })
